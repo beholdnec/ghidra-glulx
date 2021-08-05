77d37f6b8220132c40f4df4b3c8c89a630b80644
N.E.C <beholdnec@gmail.com>
2021-08-05 14:06:48 -0700

    Implement InjectPayloadGlulxParameters

--- src/main/java/glulx/pcode/InjectPayloadGlulxParameters.java
+++ src/main/java/glulx/pcode/InjectPayloadGlulxParameters.java
@@ -3,11 +3,14 @@ package glulx.pcode;
 import ghidra.app.plugin.processors.sleigh.PcodeEmit;
 import ghidra.app.plugin.processors.sleigh.SleighLanguage;
 import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressSpace;
 import ghidra.program.model.lang.InjectContext;
 import ghidra.program.model.lang.InjectPayload;
+import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.Program;
 import ghidra.program.model.pcode.PcodeOp;
 import ghidra.program.model.pcode.Varnode;
+import ghidra.util.Msg;
 import ghidra.util.xml.SpecXmlUtils;
 import ghidra.xml.XmlElement;
 import ghidra.xml.XmlParseException;
@@ -19,6 +22,12 @@ public class InjectPayloadGlulxParameters implements InjectPayload {
 	private String sourceName;
 	private InjectParameter[] noParams;
 	private boolean analysisStateRecoverable;
+	private AddressSpace constantSpace;
+	private int paramSpaceID;
+	private int localSpaceID;
+	private Varnode temp4;
+	private Varnode zero;
+	private Varnode four;
 	private Varnode LVA;
 	
 	public InjectPayloadGlulxParameters(String nm, String srcName, SleighLanguage language, long uniqBase) {
@@ -26,6 +35,23 @@ public class InjectPayloadGlulxParameters implements InjectPayload {
 		sourceName = srcName;
 		noParams = new InjectParameter[0];
 		analysisStateRecoverable = true;
+		constantSpace = language.getAddressFactory().getConstantSpace();
+		
+		AddressSpace uniqueSpace = language.getAddressFactory().getUniqueSpace();
+		Address temp4Address = uniqueSpace.getAddress(uniqBase);
+		
+		AddressSpace paramSpace = language.getAddressFactory().getAddressSpace("param");
+		paramSpaceID = paramSpace.getSpaceID();
+		AddressSpace localSpace = language.getAddressFactory().getAddressSpace("local");
+		localSpaceID = localSpace.getSpaceID();
+		
+		// create temp storage location
+		temp4 = new Varnode(temp4Address, 4);
+		
+		// create varnodes for incrementing pointer by 4 bytes
+		zero = new Varnode(constantSpace.getAddress(0), 4);
+		four = new Varnode(constantSpace.getAddress(4), 4);
+		
 		Address LVAregAddress = language.getRegister("LVA").getAddress();
 		LVA = new Varnode(LVAregAddress, 4);
 	}
@@ -76,8 +102,54 @@ public class InjectPayloadGlulxParameters implements InjectPayload {
 			return new PcodeOp[0];
 		}
 		
-		// TODO
-		return new PcodeOp[0];
+		Address funcAddr = con.baseAddr;
+		// FIXME: baseAddr points to the instruction following the C0/C1 function header,
+		// not the function itself. Therefore we must use getFunctionContaining instead of
+		// getFunctionAt. I have no idea why this happens.
+		Function function = program.getFunctionManager().getFunctionContaining(funcAddr);
+		if (function == null) {
+			Msg.info(this, "Function at " + funcAddr.toString() + " was null.");
+			return new PcodeOp[0];
+		}
+		
+		Msg.info(this, "injecting uponentry into function " + function.toString() + " at " + funcAddr.toString());
+		int numOps = function.getParameterCount();
+		Msg.info(this, "adding " + function.getParameterCount() + " parameters");
+		
+		PcodeOp[] resOps = new PcodeOp[1 + 3 * numOps];
+		int seqNum = 0;
+
+		//initialize LVA to contain 0
+		PcodeOp copy = new PcodeOp(con.baseAddr, seqNum, PcodeOp.COPY);
+		copy.setInput(zero, 0);
+		copy.setOutput(LVA);
+		resOps[seqNum++] = copy;
+		
+		Varnode tempLocation = temp4;
+		Varnode increment = four;
+		
+		for (int i = 0; i < numOps; i++) {
+			//copy value from parameterSpace to temporary
+			PcodeOp load = new PcodeOp(con.baseAddr, seqNum, PcodeOp.LOAD);
+			load.setInput(new Varnode(constantSpace.getAddress(paramSpaceID), 4), 0);
+			load.setInput(LVA, 1);
+			load.setOutput(tempLocation);
+			resOps[seqNum++] = load;
+			//copy temporary to LVA
+			PcodeOp store = new PcodeOp(con.baseAddr, seqNum, PcodeOp.STORE);
+			store.setInput(new Varnode(constantSpace.getAddress(localSpaceID), 4), 0);
+			store.setInput(LVA, 1);
+			store.setInput(tempLocation, 2);
+			resOps[seqNum++] = store;
+			//increment LVA reg 
+			PcodeOp add = new PcodeOp(con.baseAddr, seqNum, PcodeOp.INT_ADD);
+			add.setInput(LVA, 0);
+			add.setInput(increment, 1);
+			add.setOutput(LVA);
+			resOps[seqNum++] = add;
+		}
+		
+		return resOps;
 	}
 
 	@Override
